035f6064f522a5f7cc9bcbcdaf4f25d064095a99
chrisvanndy <chrisvannyc@gmail.com>
2021-04-28 21:27:17 +0000

    return None if empty

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     # if the list is empty return "None"
     if not my_list:
-        my_list = None
+        return (None)
     else:
         my_list.sort()
     return (my_list[-1])
