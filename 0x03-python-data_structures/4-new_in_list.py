9290505320c8e3ad100ab6914162067c17b54134
chrisvanndy <chrisvannyc@gmail.com>
2021-04-28 18:44:22 +0000

    list.copy() // perform operations on copy return copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx > 0:
-        list_copy = my_list
+    list_copy = my_list.copy()
+    if idx < 0:
         return (list_copy)
-    if idx > (len(my_list)):
-        list_copy2 = my_list
-        return(list_copy2)
-    if idx <= (len(my_list)) - 1:
-        list_copy3 = my_list
-        list_copy3[idx] = element
-        return (list_copy3)
+    elif idx > (len(my_list)):
+        return(list_copy)
+    elif idx <= (len(my_list)) - 1:
+        list_copy[idx] = element
+        return (list_copy)
