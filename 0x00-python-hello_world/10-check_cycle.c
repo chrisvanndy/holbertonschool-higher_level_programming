4aaef21f7ec9233a47d8458ad2770c50677edd64
chrisvanndy <chrisvannyc@gmail.com>
2021-04-29 02:32:47 +0000

    torty and harry tackle a list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * check_cycle - checks a linked list for a cycle / loop
+ * @list: pointer to linked list
+ * Return: int 0 if no loop 1 if loop detected
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *torty;
+	listint_t *harry;
+
+	torty = list;
+	harry = list;
+	
+	while(totry->next != NULL && harry->next->next != NULL)
+	{
+		torty = torty->next;
+		harry = harry->next->next;
+		if (torty == harry)
+			return (1);
+	}
+	return (0);
+}
