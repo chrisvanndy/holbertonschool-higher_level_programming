3e74f39d97c060a9c4aa3b5931bd651de70c89b3
chrisvanndy <chrisvannyc@gmail.com>
2021-05-04 15:08:05 +0000

    documentation / pep8

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" Square class gets a private variable """
+
+
 class Square:
-    def __init__(self, size): 
+    """ Square is a class which contains instances and methods related to
+    the class type square """"
+    def __init__(self, size):
         self.__size = size
