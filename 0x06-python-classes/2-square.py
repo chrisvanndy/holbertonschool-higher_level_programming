ac6600d2450241eea50f582df5d6551391a27005
chrisvanndy <chrisvannyc@gmail.com>
2021-05-04 15:13:56 +0000

    pep8 / documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+""" Square gets private variable size instantiated with value 0 """
+
+
 class Square:
-    def __init__(self, size = 0):
+    """ Square class type initializes with variable size to 0
+    value checks are implemented to raise errors before setting
+    the value of the variable """
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
