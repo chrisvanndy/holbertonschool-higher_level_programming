55e6bdf0701392fc88965eee5a903bc9c829cce6
chrisvanndy <chrisvannyc@gmail.com>
2021-05-04 15:27:11 +0000

    pep8 / documentation / formatting

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
+""" Class Square has instantiated variable size, @property decorator
+    to return size, @size.setter to set size if errors dont arise
+    and an area function to determine area of a "square"
+    """
+
+
 class Square:
     """ Square contains perameters which define the class type square
     """
-   
-    def __init__(self, size = 0):
-      #  if not isinstance(size, int):
-          #  raise TypeError("size must be an integer")
-     #   if size < 0:
-       #     raise ValueError("size must be >= 0")
-       
-        self.__size = size
+    def __init__(self, size=0)
+    self.__size = size
 
     @property
     def size(self):
+        """ size() is a getter function
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ size(self, value) is the setter function with perameters
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.value = value
-     
+            self.__size = value
+
     def area(self):
-        """ Returns the area of class type square""" 
+        """ Returns the area of class type square
+        """
         area = self.__size ** 2
         return area
