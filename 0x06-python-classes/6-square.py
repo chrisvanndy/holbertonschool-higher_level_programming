24565f2b2758a3c1643ed00806247274fb5f907d
chrisvanndy <chrisvannyc@gmail.com>
2021-05-04 20:47:57 +0000

    re-added private declarations

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,8 +9,8 @@ class Square:
     """ Square contains perameters which define the class type square
     """
     def __init__(self, size=0, position=(0, 0)):
-        self.size = size
-        self.position = position
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
