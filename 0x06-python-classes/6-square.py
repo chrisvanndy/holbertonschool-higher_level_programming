e59b728bf85660ce6a5167f54fcab62ca8937f29
chrisvanndy <chrisvannyc@gmail.com>
2021-05-04 20:46:42 +0000

    remove private qualifier in init initializations

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,8 +9,8 @@ class Square:
     """ Square contains perameters which define the class type square
     """
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
