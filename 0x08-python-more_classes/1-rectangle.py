63b37442d366267784f899232ba3729bbf11e3bb
chrisvanndy <chrisvannyc@gmail.com>
2021-05-05 22:02:20 +0000

    order type / value

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """ Class Rectangle """
     __width = 0
     __height = 0
-    
+
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -19,26 +19,27 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ width setter sets private variable based on parameters """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
         if isinstance(value, int):
             if value < 0:
                 raise ValueError("width must be >= 0")
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")    
         else:
             self.__width = value
-    
+
     @property
     def height(self):
         """ height() is a property of instance height """
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """ height setter sets private variable based on parameters """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
         if isinstance(value, int):
             if value < 0:
                 raise ValueError("height must be >= 0")
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer") 
         else:
             self.__height = value
