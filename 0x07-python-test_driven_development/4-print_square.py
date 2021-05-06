caae54306d0c04a15fe15cfd059368f7a8da1d56
chrisvanndy <chrisvannyc@gmail.com>
2021-05-06 18:57:10 +0000

    pep8 and docs

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/pyton3
+""" Documentation for module print_square"""
+
+
 def print_square(size):
+    """ print_square handles printing of square under certain conditions """
     if isinstance(size, int):
         if size < 0:
             raise ValueError("size must be >=0")
         if isinstance(size, float):
             if size < 0:
                 raise ValueError("size must be an integer")
-    else:   
+    else:
         raise TypeError("size must be an integer")
-    
+
     for row in range(size):
         for row in range(size):
             print("#", end='')
         print()
-  
