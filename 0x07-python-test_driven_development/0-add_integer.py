b2ad6182f55b244790dc83379d7812f1f1a724d4
chrisvanndy <chrisvannyc@gmail.com>
2021-05-07 19:45:06 +0000

    handle inf and overflow

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,9 +5,16 @@
 def add_integer(a, b=98):
     """ add_integer function receives values and adds
     """
+    # determine a and be are int or float
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # handle float values
+    if a == float('inf'):
+        raise TypeError("a must be an integer")
+    if b == float('inf'):
         raise TypeError("b must be an integer")
 
     if isinstance(a, float):
