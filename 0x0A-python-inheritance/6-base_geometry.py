1384bc5e9217838b9942a410db68f461faf370b2
chrisvanndy <chrisvannyc@gmail.com>
2021-05-11 20:07:13 +0000

    introduce publice instance method area()

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define BaseGeometry, an empty class"""
+
+
+class BaseGeometry():
+    """ BaseGeometry is the super() class """
+    pass
+
+    def area(self):
+        """Returns the area of a shape"""
+        raise Exception("area() is not implemented")
