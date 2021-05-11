6d63171a842b4cca2d549444bbb515a118711191
chrisvanndy <chrisvannyc@gmail.com>
2021-05-11 19:36:19 +0000

    tests instances of objects class, returns T or F

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" is_same_class takes an object and class and returns True or False"""
+
+
 def is_same_class(obj, a_class):
+    """ is_same_classe returns true or false based on instance of object
+    """
     if isinstance(obj, a_class) and type(obj) == a_class:
         return True
     else:
