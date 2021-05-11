4c7fa7889c909c59dcd5f175b8ce03e553781174
chrisvanndy <chrisvannyc@gmail.com>
2021-05-11 20:26:01 +0000

    validate input

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define BaseGeometry, an empty class"""
+
+
+class BaseGeometry():
+    """ BaseGeometry is the super() class """
+    pass
+
+    def area(self):
+        """Returns the area of a shape"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value passed to method"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
