e085e4c7f2e33c94fe2f068940a6065188136439
chrisvanndy <chrisvannyc@gmail.com>
2021-05-11 19:20:45 +0000

    print sorted lists

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" MyList will inherit from list and return ordered list """
+
+
+class MyList(list):
+    """ MyList is super() class of this module """ 
+        
+        
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
+        
