88f396b917d3cf157162741b95d1ffb0dd17f57d
chrisvanndy <chrisvannyc@gmail.com>
2021-05-11 21:13:37 +0000

    import BaseGeometry from task 7

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,19 +2,7 @@
 """Define BaseGeometry, an empty class"""
 
 
-class BaseGeometry:
-    """ BaseGeometry is the super() class """
-
-    def area(self):
-        """Returns the area of a shape"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates value passed to method"""
-        if not isinstance(value, int) or type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
