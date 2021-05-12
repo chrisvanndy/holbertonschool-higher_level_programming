39672036db21406e5d42b9af597a04ca77801aa8
chrisvanndy <chrisvannyc@gmail.com>
2021-05-12 16:27:26 +0000

    doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" read_file opens a file and prints to stdout """
+
+
 def read_file(filename=""):
+    """ read_file opens "filename" passed from main and prints """
     with open(filename, mode="r", encoding="utf-8") as text:
         print(text.read(), end="")
