d3e389e6e7c3077f1c80a6dd9e0e96a420ee7066
chrisvanndy <chrisvannyc@gmail.com>
2021-05-14 21:48:58 +0000

    initialize square with width and height

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,8 +10,25 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y,  id)
+        self.size = size
 
     def __str__(self):
         sqr = "[Square] ({}) {}".format(self.id, self.x)
-        sqr += "/{} - {}".format(self.y, self.width)
+        sqr += "/{} - {}".format(self.y, self.size)
         return sqr
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+            self.height = value
