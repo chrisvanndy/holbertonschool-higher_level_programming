1b0ecd9164826a14bffc465d76ad544005604d76
chrisvanndy <chrisvannyc@gmail.com>
2021-05-14 16:06:30 +0000

    instantiate id, and __nb_objects

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define class Base: and its attribute __nb_objects"""
+
+
+class Base:
+    """Base is a class which will be expanded in future tasks"""
+    #private class attribute nb_objects is incrementation var
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+        if id is not None: 
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+         
