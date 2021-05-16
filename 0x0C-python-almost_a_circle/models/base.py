325f23f927ebedbce5e76d77e637f5c61d1d6941
chrisvanndy <chrisvannyc@gmail.com>
2021-05-16 19:11:00 +0000

    import json // json.dumps(passed dict) that meets given parameters

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Define class Base: and its attribute __nb_objects"""
 
+import json
+
 
 class Base:
     """Base is a class which will be expanded in future tasks"""
@@ -13,3 +15,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """to_json_string converts dicts to json string"""
+        json_str = []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return json_str
+        json_str = json.dumps(list_dictionaries)
+        return json_str
