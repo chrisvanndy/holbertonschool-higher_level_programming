2809e781db99a6176210a22bde4bbc6abf21d28b
chrisvanndy <chrisvannyc@gmail.com>
2021-05-16 21:23:27 +0000

    class method // ipmorts other functions to Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,23 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    # will reaquire cls instead of self
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save_to_file writes the json rep str "list_objs" to a file"""
+        new_file = cls.__name__ + ".json"
+        the_list = []
+        if list_objs:
+            for objects in list_objs:
+                dict_obj = cls.to_dictionary(objects)
+                the_list.append(dict_obj)
+                # now a list of dictionaries exits ^^ to pass to to_json_string
+            json_str = cls.to_json_string(the_list)
+        else:
+            json_str = "[]"
+        with open(new_file, 'w') as new:
+            new.write(json_str)
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """to_json_string converts dicts to json string"""
