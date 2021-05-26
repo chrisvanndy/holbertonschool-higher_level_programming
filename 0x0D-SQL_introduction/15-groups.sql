de3ac05593fe070d659b5f106ac691eb60f31f65
chrisvanndy <chrisvannyc@gmail.com>
2021-05-26 18:37:06 +0000

    GROUP BY score ORDER BY number DESC

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- write script that returns number or records with matching values
 -- show results as score / score number /count:
-SELECT score, COUNT(score) AS number FROM second_table;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
