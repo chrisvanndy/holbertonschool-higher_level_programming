493f0ccb12d142d427c97be245a447d79f62a5b3
chrisvanndy <chrisvannyc@gmail.com>
2021-05-27 17:26:11 +0000

    database not properly instanciated

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- Create database hbtn_0d_2, if it doesn't exist
 -- Create new user user_0d_2 with password user_0d_2_pwd, if not exists
 -- GRANT SELETCT(read only) permissions to the specified user
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
