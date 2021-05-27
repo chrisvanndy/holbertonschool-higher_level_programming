47572d447c253d1201d84e5c9e396a37e4e9e783
chrisvanndy <chrisvannyc@gmail.com>
2021-05-27 19:37:33 +0000

    extra ; in code GAHHH

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,5 @@ SELECT s.`title`, g.`genre_id`
 FROM `tv_shows` AS s
 -- tv_show attr name id(primarykey) tv_show_genre attr show id(fk?) genre_id)
 -- tv_show.id and tv_show_genre.show_id ( are  == ) inner join these attrs
-	INNER JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id`;
+	INNER JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id`
 ORDER BY s.`title`, g.`genre_id`;
