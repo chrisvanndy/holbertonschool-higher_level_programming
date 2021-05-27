76df626437dac64205d2ba8abc00efcfdada2e28
chrisvanndy <chrisvannyc@gmail.com>
2021-05-27 17:24:50 +0000

    syntax missing < ; >

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 -- GRANT SELETCT(read only) permissions to the specified user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
