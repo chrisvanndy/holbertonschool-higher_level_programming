24cf35ebb2d9e8376ce77547a11e7ad5aa9630c9
chrisvanndy <chrisvannyc@gmail.com>
2021-05-27 19:46:31 +0000

    left join returns all records from left table, and matched records from right

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- write a script that lists all shows contained in database
+-- output: tv_shows.title - tv_show_genres.genre_id
+-- output: tv_shows.title ascending and tv_show_genres.genre_id
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+-- tv_show attr name id(primarykey) tv_show_genre attr show id(fk?) genre_id)
+-- tv_show.id and tv_show_genre.show_id ( are  == ) inner join these attrs
+	LEFT JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id`
+ORDER BY s.`title`, g.`genre_id`;
