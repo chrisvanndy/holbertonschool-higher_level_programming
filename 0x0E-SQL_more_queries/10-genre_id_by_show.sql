f838745ef291ef639686d5323560e88bb618f686
chrisvanndy <chrisvannyc@gmail.com>
2021-05-27 19:30:03 +0000

    spelling error

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- output: tv_shows.title - tv_show_genres.genre_id
 -- output: tv_shows.title ascending and tv_show_genres.genre_id
 SELECT s.'title', g.'genre_id'
-FROM 'tv_show' AS s
+FROM 'tv_shows' AS s
 -- tv_show attr name id(primarykey) tv_show_genre attr show id(fk?) genre_id)
 -- tv_show.id and tv_show_genre.show_id ( are  == ) inner join these attrs
 	INNER JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id`;
