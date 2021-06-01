375491d748d51ef981cc49c44eeec77fe110cfa0
chrisvanndy <chrisvannyc@gmail.com>
2021-06-01 18:35:32 +0000

    learning alchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Create an engine"""
+
+from sys import argv
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], 
+                           argv[2], 
+                           argv[3], 
+                           pool_pre_ping=True))
+    
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    sesh = Session()
+
+    state = sesh.query(State)
+
+    for st in state:
+        print("{}: {}".format(st.id, st.name))
+
