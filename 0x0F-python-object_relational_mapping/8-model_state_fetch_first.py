6eab3eff71130d081555c04c290a61caddf7f863
chrisvanndy <chrisvannyc@gmail.com>
2021-06-01 18:44:46 +0000

    access an object with .first()

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Create an engine"""
+
+from sys import argv
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1],
+                                   argv[2],
+                                   argv[3],
+                                   pool_pre_ping=True))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    sesh = Session()
+
+    state = sesh.query(State).first()
+
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
