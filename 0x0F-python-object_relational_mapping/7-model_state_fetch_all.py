b0979ecea77cb37f2de0dd47b20064ddd1e31251
chrisvanndy <chrisvannyc@gmail.com>
2021-06-01 18:37:29 +0000

    learning alchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,11 +9,11 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], 
-                           argv[2], 
-                           argv[3], 
-                           pool_pre_ping=True))
-    
+                           .format(argv[1],
+                                   argv[2],
+                                   argv[3],
+                                   pool_pre_ping=True))
+
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
@@ -24,4 +24,3 @@ if __name__ == "__main__":
 
     for st in state:
         print("{}: {}".format(st.id, st.name))
-
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,7 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+
     """Introduces State Class which inherit from Base)"""
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, unique=True, nullable=True)
