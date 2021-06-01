0ad0ca9ed5ba42bd5b73bc9503f45a805d06ea34
chrisvanndy <chrisvannyc@gmail.com>
2021-06-01 19:11:35 +0000

    create new state add to database, print id

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Create an engine"""
+
+from sys import argv
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1],
+                                   argv[2],
+                                   argv[3],
+                                   pool_pre_ping=True))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    sesh = Session()
+
+    newstate = State(name="Louisiana")
+
+    sesh.add(newstate)
+
+    sesh.commit()
+
+    print(newstate.id)
