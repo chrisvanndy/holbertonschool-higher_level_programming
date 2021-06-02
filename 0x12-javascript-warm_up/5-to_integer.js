bfd3852485eaadbbaa903823661410c40c6c7d2a
chrisvanndy <chrisvannyc@gmail.com>
2021-06-02 19:27:36 +0000

    NaN not parseint()

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv.slice(2);
+
+/* slice two removes node and file path args[0] IS first arg */
+/* if one exists */
+
+if (typeof args[0] === 'undefined' || isNaN(args[0])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: %i', args[0]);
+}
