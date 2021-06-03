ea61bef361545525d8b6358b0e4231d16b5c1961
chrisvanndy <chrisvannyc@gmail.com>
2021-06-03 22:05:03 +0000

    inherit via different name, don't return define x

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const Squirt = require('./5-square');
+
+class Square extends Squirt {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
