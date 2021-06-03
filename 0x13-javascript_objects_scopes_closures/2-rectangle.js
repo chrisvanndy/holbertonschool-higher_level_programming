2f3b677e7d3dc0672ff8078d22c936aba546a250
chrisvanndy <chrisvannyc@gmail.com>
2021-06-03 17:40:05 +0000

    account for undefined + semistandard

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,22 +5,18 @@ class Rectangle {
     this.width = w;
     this.height = h;
 
-    if (w <= 0) {
+    if (w <= 0 || w === undefined) {
       this.width = 0;
       this.height = 0;
     } else {
       this.width = w;
-     }
-    if (h <= 0) {
+    }
+    if (h <= 0 || h === undefined) {
       this.width = 0;
       this.height = 0;
     } else {
       this.height = h;
     }
-  }
-}
-module.exports = Rectangle;
-
   }
 }
 module.exports = Rectangle;
