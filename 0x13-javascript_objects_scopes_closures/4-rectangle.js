b34f8a5f766100bd3ee6e1d22c160a620336e972
chrisvanndy <chrisvannyc@gmail.com>
2021-06-03 20:12:51 +0000

    add methods to class Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print (i, j) {
+    let str = '';
+    for (i = 0; i < this.width; i++) {
+      str += 'X';
+    }
+    for (j = 0; j < this.height; j++) {
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
